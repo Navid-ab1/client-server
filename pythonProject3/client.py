ba1e644dbfff65ede6f1418ea503a41bb60f4966
Navid-ab1 <navidab3113@gmail.com>
2024-10-06 16:22:29 +0330

    Complete the client side

---
+++ pythonProject3/client.py
@@ -0,0 +1,30 @@
+import zmq
+import json
+
+context = zmq.context()
+socket = context.socket(zmq.req)
+socket.connect('tcp://localhost:5555')
+
+def send_request(command):
+    socket.send_string(json.dumps(command))
+    response = socket.recv_string()
+    print(f'Response of the server is {response}')
+
+
+os_command = {
+    "command_type":"os",
+    "command":"ls",
+    "args":["-l"]
+}
+
+
+math_command = {
+    "command_type":"math",
+    "operation":"math",
+    "operands":[5,10]
+}
+
+print("Sending os command ")
+send_request(os_command)
+print("Sending math command")
+send_request(math_command)
